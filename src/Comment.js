8341f889ec8f6dc7aa19864be4536b737aba50de
Bakeloh <bakelohnoel@gmail.com>
2023-12-05 12:28:58 +0300

    Completed Assignment

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,16 @@ function Comment() {
 }
 
 export default Comment;
+
+// Or using the arrow function syntax:
+
+// const Comment = () => <div>Naturally, I agree with this article.</div>;
+
+
+// A class component looks like this:
+
+// class Comment extends React.Component {
+//   render() {
+//     return <div>Naturally, I agree with this article.</div>;
+//   }
+// }
